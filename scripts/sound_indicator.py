149f32be5df0a07c032a46ff1e0a9645501616c2
arunavanag591 <arunavoster@gmail.com>
2020-09-29 17:44:48 -0700

    sound node is working

--- scripts/sound_indicator.py
+++ scripts/sound_indicator.py
@@ -11,24 +11,32 @@ from sound_play.libsoundplay import SoundClient
 soundhandle = SoundClient()
 
 def sound_play(msg):
-    
+    # soundhandle.play(1,1)    
+    # rospy.loginfo("%f" %msg.data)
+    # rospy.sleep(1)
     if(msg.data >=0 or msg.data <= 2):
-        return soundhandle.playWave("~/Downloads/test.wav", 0.2)
+        soundhandle.play(1, 0.2)
+        rospy.sleep(1)
     elif(msg.data > 2 or msg.data <= 4):
-        return soundhandle.playWave("/home/ecc/Downloads/test.wav", 0.4)
+        soundhandle.play(1, 0.4)
+        rospy.sleep(1)
     elif(data > 4 or data <= 6):
         print("Voltage is between 4-6")
-        return soundhandle.playWave("/home/ecc/Downloads/test.wav", 0.6)
+        soundhandle.play(1, 0.6)
+        rospy.sleep(1)
     elif(data > 6.0 or data <= 8.0):
         print("Voltage is between 6-8")
-        return soundhandle.playWave("/home/ecc/Downloads/test.wav", 0.8)
+        soundhandle.play(1, 0.8)
+        rospy.sleep(1)
     elif(data > 8 or data <= 10.0):
-        return soundhandle.playWave("/home/ecc/Downloads/test.wav", 1.0)
+        soundhandle.play(1, 1.0)
+        rospy.sleep(1)
+    
+    
 
 def initialize():
-    rospy.init_node('mynode', anonymous=True)
-    
-    rospy.Subscriber("analog_output", Float64, sound_play)
+    rospy.init_node('mynode', anonymous=True)   
+    rospy.Subscriber("analog_output", Float64, sound_play)  
     rospy.spin()
     
     
